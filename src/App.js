db4734b7d5b5592443983eb1bf4a67a7b8b88ca4
JuliusKilanko <juliuskilanko25@gmail.com>
2023-03-25 21:14:43 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Login from "./pages/Login/Login";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [notLoddedIn, setNotLoggedIn] = useState(true)
+  const [isValid, setIsVaid] = useState(false)
+
   function handleLoggedIn(e){
     e.preventDefault()
     setIsLoggedIn(true)
-    alert(isLoggedIn)
+    setNotLoggedIn(false)
   }
   return (
     <React.Fragment>
       {isLoggedIn && <Home />}
-      <Login onClick={handleLoggedIn}/>
+      {notLoddedIn && <Login onClick={handleLoggedIn}/>}
     </React.Fragment>
   );
 }
